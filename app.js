142fa8ffc11dfd674e65f89d3187e74b6802de4d
Dawson Toth <dtoth@appcelerator.com>
2014-11-18 14:59:13 -0500

    API Usage Consistency

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var APIBuilder = require('apibuilder'),
 	server = new APIBuilder(),
-	Connector = require('./lib').create(APIBuilder, server),
+	ConnectorFactory = require('./lib'),
+	Connector = ConnectorFactory.create(APIBuilder, server),
 	connector = new Connector();
 
 // lifecycle examples
